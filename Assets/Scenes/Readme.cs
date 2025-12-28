18abf86934aed97bf453fdcb991eea0f42fba5cb
Melisa Lai <melisa03wanjiun@gmail.com>
2025-12-28 14:32:39 +0800

    edit readme

--- Assets/Scenes/Readme.cs
+++ Assets/Scenes/Readme.cs
@@ -6,4 +6,5 @@ public class Readme
     // after opening, main menu and ending are done
     // they will be removed and replaced by a single main camera in boot (dont forget camerafollow2d)
     // so, u see all the layer i put scene init for each layer
+    //the end
 }
